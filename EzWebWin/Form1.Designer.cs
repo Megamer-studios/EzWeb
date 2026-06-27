554f24799d0665d8dbdda5598be4865725433229
Aquamarine Hoshino <cozoral@outlook.com>
2026-06-27 16:11:33 +0200

    Add project files.

---
+++ EzWebWin/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace EzWebWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            numericUpDown2 = new NumericUpDown();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            menuStrip1 = new MenuStrip();
+            basicTutorialToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(6, 26);
+            numericUpDown2.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(183, 27);
+            numericUpDown2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(numericUpDown2);
+            groupBox1.Location = new Point(12, 34);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(195, 64);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Port";
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.Location = new Point(18, 367);
+            button1.Name = "button1";
+            button1.Size = new Size(183, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Start server";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.BackColor = SystemColors.ControlLight;
+            textBox1.Location = new Point(213, 34);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(307, 397);
+            textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(18, 104);
+            button2.Name = "button2";
+            button2.Size = new Size(183, 29);
+            button2.TabIndex = 6;
+            button2.Text = "Choose path";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Enabled = false;
+            textBox2.Location = new Point(12, 139);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(195, 103);
+            textBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button3.Enabled = false;
+            button3.Location = new Point(18, 402);
+            button3.Name = "button3";
+            button3.Size = new Size(183, 29);
+            button3.TabIndex = 8;
+            button3.Text = "Stop";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            pictureBox1.Image = Properties.Resources.EzWeb;
+            pictureBox1.Location = new Point(18, 248);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(183, 113);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { basicTutorialToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(532, 28);
+            menuStrip1.TabIndex = 10;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // basicTutorialToolStripMenuItem
+            // 
+            basicTutorialToolStripMenuItem.Name = "basicTutorialToolStripMenuItem";
+            basicTutorialToolStripMenuItem.Size = new Size(109, 24);
+            basicTutorialToolStripMenuItem.Text = "Basic tutorial";
+            basicTutorialToolStripMenuItem.Click += basicTutorialToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(64, 24);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(532, 443);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(textBox2);
+            Controls.Add(menuStrip1);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            MinimumSize = new Size(550, 490);
+            Name = "Form1";
+            Text = "EzWeb";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private NumericUpDown numericUpDown2;
+        private GroupBox groupBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private TextBox textBox2;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem basicTutorialToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ EzWebWin/Form1.cs
@@ -0,0 +1,224 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace EzWebWin
+{
+    public partial class Form1 : Form
+    {
+
+        int port;
+        bool hosting = false;
+        HttpListener listener = new HttpListener();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void newLine(string a)
+        {
+            textBox1.AppendText(Environment.NewLine + a);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                port = (int)numericUpDown2.Value;
+
+
+                listener.Prefixes.Add($"http://*:{port}/");
+                newLine($"Starting server on port {port}...");
+                listener.Start();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = $"http://localhost:{port}/",
+                    UseShellExecute = true
+                };
+                Process.Start(startInfo);
+                newLine($"Listening for connections on http://+:{port}/");
+                newLine($"Hosted on http://localhost:{port}/");
+                hosting = true;
+                numericUpDown2.Enabled = false;
+                button1.Enabled = false;
+                button3.Enabled = true;
+                while (hosting)
+                {
+
+                    HttpListenerContext context = await listener.GetContextAsync();
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+                    string path = request.Url.AbsolutePath;
+                    string rootFolder = folderBrowserDialog1.SelectedPath;
+
+
+                    string templatePath = Path.Combine(rootFolder, "layout.html");
+                    string notFoundPath = Path.Combine(rootFolder, "notFound.html");
+
+                    string targetFilePath;
+                    string targetedFilePath;
+                    if (path == "/")
+                    {
+                        targetFilePath = Path.Combine(rootFolder, "index.html");
+                        targetedFilePath = targetFilePath;
+                    }
+                    else
+                    {
+                        targetFilePath = Path.Combine(rootFolder, path.TrimStart('/'));
+                        targetedFilePath = targetFilePath;
+
+                        if (!File.Exists(targetFilePath) && string.IsNullOrEmpty(Path.GetExtension(targetFilePath)))
+                        {
+                            targetFilePath += ".html";
+                            targetedFilePath = targetFilePath;
+                        }
+                    }
+
+                    string pageContent = "";
+                    string webContent = "";
+                    string extension = Path.GetExtension(targetedFilePath).ToLower();
+                    byte[] buffer;
+                    if (extension == ".html")
+                    {
+                        try
+                        {
+                            string layoutText = File.ReadAllText(templatePath);
+
+                            if (targetedFilePath != templatePath)
+                            {
+                                if (File.Exists(targetedFilePath))
+                                {
+                                    pageContent = File.ReadAllText(targetedFilePath);
+
+                                }
+                                else if (File.Exists(notFoundPath))
+                                {
+                                    pageContent = File.ReadAllText(notFoundPath);
+                                    response.StatusCode = 404;
+                                }
+                                else
+                                {
+                                    pageContent = "<h1>404 Not Found</h1>";
+                                    response.StatusCode = 404;
+                                }
+                            }
+                            else
+                            {
+                                pageContent = File.ReadAllText(notFoundPath);
+                            }
+
+
+
+
+
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            webContent = $"<h1>Internal Server Error</h1><p>{ex.Message}</p>";
+                            newLine($"Error reading files: {ex.Message}");
+                        }
+                        if (File.Exists(templatePath))
+                        {
+                            webContent = File.ReadAllText(templatePath).Replace("{WebContent}", pageContent);
+                        }
+                        else
+                        {
+                            webContent = pageContent;
+                        }
+                        buffer = System.Text.Encoding.UTF8.GetBytes(webContent);
+                        response.ContentType = "text/html";
+                    }
+                    else
+                    {
+                        buffer = File.ReadAllBytes(targetedFilePath);
+                    }
+
+
+
+
+
+                    //string currentPath = Path.Combine(folderBrowserDialog1.SelectedPath, "/" + path.TrimStart('/'));
+                    //newLine(currentPath);
+                    //string webContent = "";
+
+                    //    string a = File.ReadAllText(templatePath);
+                    //    string b = File.ReadAllText(folderBrowserDialog1.SelectedPath + path.TrimStart('/'));
+                    //    string extension = Path.GetExtension(currentPath).ToLower();
+                    //    if (extension == ".html")
+                    //    {
+                    //        b = File.ReadAllText(folderBrowserDialog1.SelectedPath + '/' + path.TrimStart('/') + ".html");
+                    //    }
+                    //    if (path == "/")
+                    //    {
+                    //        b = File.ReadAllText(folderBrowserDialog1.SelectedPath + "/index.html");
+                    //    }
+                    //    if (!File.Exists(currentPath))
+                    //   {
+                    //    b = File.ReadAllText(folderBrowserDialog1.SelectedPath + "/notFound.html");
+                    //}
+                    //    webContent = a.Replace("{WebContent}", b);
+                    //    newLine(webContent);
+
+
+
+
+
+                    response.ContentLength64 = buffer.Length;
+
+                    System.IO.Stream output = response.OutputStream;
+                    await output.WriteAsync(buffer, 0, buffer.Length);
+                    output.Close();
+
+                    Application.DoEvents();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (hosting)
+                {
+                    newLine($"Error: {ex.Message}");
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numericUpDown2.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            hosting = false;
+            listener.Stop();
+            newLine($"Server stopped.");
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox1 = new AboutBox1();
+            aboutBox1.Show();
+        }
+
+        private void basicTutorialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Info info = new Info();
+            info.Show();
+        }
+    }
+}
+
